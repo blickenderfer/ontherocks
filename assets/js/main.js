dd319f20202e648c3b2156bc756b43531b3db9ea
blickenderfer <>
2023-05-01 17:46:43 -0400

    added conversation starter API, continued building modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,9 @@
 var search = document.querySelector("#search-container")
 var inputBar = document.querySelector("#search-bar")
 var key = `1`
+var factEl = document.querySelector("#fact");
 
-const fetchUrl ="https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+const fetchUrl = "https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
 
 // fetch(fetchUrl)
 //  .then(function(response){
@@ -27,18 +28,31 @@ searchBtn.addEventListener("click", (event) => {
     var modalTitle = document.querySelector(".modal-card-title");
     var searchDrink = document.querySelector("#search-bar").value;
     console.log(searchDrink);
-    let fetchUrl =`https://thecocktaildb.com/api/json/v1/1/search.php?s=${searchDrink}`
+    let fetchUrl = `https://thecocktaildb.com/api/json/v1/1/search.php?s=${searchDrink}`
     fetch(fetchUrl)
- .then(function(response){
-    return response.json();
- })
-.then(function(data){
-    console.log(data.drinks);
-    modalTitle.textContent=data.drinks[0].strDrink;
-    var drinkImg = document.querySelector("#drink-img"); 
-    drinkImg.src=data.drinks[0].strDrinkThumb;
-    //make a loop to check if a particular ingredient is null in order to include it 
-})
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.drinks);
+            var topResult = data.drinks[0]
+            modalTitle.textContent = data.drinks[0].strDrink;
+            var drinkImg = document.querySelector("#drink-img");
+            drinkImg.src = data.drinks[0].strDrinkThumb;
+            //make a loop to check if a particular ingredient is null in order to include it 
+            var ingredients = []
+            for(i = 1; i <= 15; i++){
+                var currentIngredient = `strIngredient${i}`
+                var ingredient = topResult[currentIngredient];
+                console.log(ingredient);
+                if (!ingredient){
+                    return;
+                } else {
+                    ingredients.push(ingredient);
+                }
+            }
+            console.log(ingredients);
+        })
 });
 closeBtn.addEventListener("click", () => {
     modal.classList.remove("is-active");
@@ -53,6 +67,23 @@ cancelBtn.addEventListener("click", () => {
 
 
 /*script for conversation generator box*/
-var generateBtn = document.querySelector(".generate");
-var factContainer = document.querySelector(".fact-container");
+const generateBtn = document.querySelector("#generate");
+const factContainer = document.querySelector(".fact-container");
 
+generateBtn.addEventListener("click", (event) => {
+    fetch(`https://uselessfacts.jsph.pl/api/v2/facts/random`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            console.log(data.text);
+            factEl.textContent = data.text;
+        })
+
+
+
+
+
+
+})
