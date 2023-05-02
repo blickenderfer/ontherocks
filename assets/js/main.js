d48cd79dfe9d128cba6ec6548ec8f0437bb64dfb
Joey Pham <joeypham@joeys-mbp.mynetworksettings.com>
2023-05-01 21:35:55 -0400

    added drink grid to modal again

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,8 @@ var search = document.querySelector("#search-container")
 var inputBar = document.querySelector("#search-bar")
 var key = `1`
 var factEl = document.querySelector("#fact");
-var descriptionEl = document.querySelector(".modal .drink-description");
+var descriptionEl = document.querySelector(".drink-modal .drink-description");
 const fetchUrl = "https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
-
-// fetch(fetchUrl)
-//  .then(function(response){
-//     return response.json();
-//  })
-// .then(function(data){
-//     console.log(data.drinks);
-// })
 
 
 /*modal script*/
@@ -20,7 +12,7 @@ var closeBtn = document.querySelector("#close-button");
 var modal = document.querySelector(".modal");
 var modalBg = document.querySelector(".modal-background");
 var cancelBtn = document.querySelector("#cancel");
-var faveBtn = document.querySelector("#fave-button")
+var faveBtn = document.querySelector("#fave-button");
 
 searchBtn.addEventListener("click", (event) => {
     event.preventDefault();
@@ -39,7 +31,6 @@ searchBtn.addEventListener("click", (event) => {
             modalTitle.textContent = data.drinks[0].strDrink;
             var drinkImg = document.querySelector("#drink-img");
             drinkImg.src = data.drinks[0].strDrinkThumb;
-            //make a loop to check if a particular ingredient is null in order to include it 
             var ingredients = []
             for(i = 1; i <= 15; i++){
                 var currentIngredient = `strIngredient${i}`
@@ -51,9 +42,7 @@ searchBtn.addEventListener("click", (event) => {
                     ingredients.push(ingredient);
                     var descriptionP = document.createElement("p")
                     descriptionP.textContent = ingredient;
-                    descriptionEl.appendChild(descriptionP);
-                
-                   
+                    descriptionEl.appendChild(descriptionP);                  
                 }
             }
             console.log(ingredients);
@@ -70,6 +59,17 @@ cancelBtn.addEventListener("click", () => {
 });
 
 
+/*adding to favorites*/
+// faveBtn.addEventListener("click", () => {
+//     function saveDrink(){
+//     var key = saveDrink.id;
+//     var drinkData = 
+//     window.localStorage.setItem(key, drinkData);        
+//     }        
+//     saveDrink();
+// })
+    //drink data --> setitem for local storage and then getitem
+
 
 /*script for conversation generator box*/
 const generateBtn = document.querySelector("#generate");
@@ -85,14 +85,47 @@ generateBtn.addEventListener("click", (event) => {
             console.log(data.text);
             factEl.textContent = data.text;
         })
+    })
 
+var imgBtn = document.querySelectorAll(".searchImg")
+for ( var i = 0; i < imgBtn.length; i++){
+    imgBtn[i].addEventListener("click", function(event){
+        event.preventDefault()
+        imgSearch(this.alt)
+    })
+}
 
-
-
-
-
-//         }
-        
-//         )
-//     }
-});
+function imgSearch(searchDrink){
+    // event.preventDefault();
+    modal.classList.add("is-active");
+    var modalTitle = document.querySelector(".modal-card-title");
+    // var searchDrink = document.querySelector("#search-bar").value;
+    console.log(searchDrink);
+    let fetchUrl = `https://thecocktaildb.com/api/json/v1/1/search.php?s=${searchDrink}`
+    fetch(fetchUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.drinks);
+            var topResult = data.drinks[0]
+            modalTitle.textContent = data.drinks[0].strDrink;
+            var drinkImg = document.querySelector("#drink-img");
+            drinkImg.src = data.drinks[0].strDrinkThumb;
+            var ingredients = []
+            for(i = 1; i <= 15; i++){
+                var currentIngredient = `strIngredient${i}`
+                var ingredient = topResult[currentIngredient];
+                console.log(ingredient);
+                if (!ingredient){
+                    return;
+                } else {
+                    ingredients.push(ingredient);
+                    var descriptionP = document.createElement("p")
+                    descriptionP.textContent = ingredient;
+                    descriptionEl.appendChild(descriptionP);                  
+                }
+            }
+            console.log(ingredients);
+        })
+}   
