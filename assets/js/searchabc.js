705febdd7188634d772f6dff243a0716ecb80ee4
Lykrin-X <amir.jones427@gmail.com>
2023-05-02 19:50:58 -0400

    Changed anchor tags to buttons

--- assets/js/searchabc.js
+++ assets/js/searchabc.js
@@ -1,10 +1,44 @@
 // order an array of objects with name
-users.sort(function (a, b) {
-    if (a.name < b.name) {
-      return -1;
-    }
-    if (a.name > b.name) {
-      return 1;
-    }
-    return 0;
+// users.sort(function (a, b) {
+//     if (a.name < b.name) {
+//       return -1;
+//     }
+//     if (a.name > b.name) {
+//       return 1;
+//     }
+//     return 0;
+//   });
+
+function makeList(){
+    document.getElementById("")
+}
+
+var listBtn = document.querySelector(".list");
+var closeBtn = document.querySelector("#close-button");
+var modal = document.querySelector(".modal");
+var modalBg = document.querySelector(".modal-background");
+var cancelBtn = document.querySelector("#cancel");
+  
+  listBtn.addEventListener("click", (event) => {
+      event.preventDefault();
+      modal.classList.add("is-active");
+      var modalTitle = document.querySelector(".modal-card-title");
+      let fetchUrl = `https://thecocktaildb.com/api/json/v1/1/search.php?s=${}`
+      fetch(fetchUrl)
+          .then(function (response) {
+              return response.json();
+          })
+          .then(function (data) {
+              console.log(data.drinks);
+        })
+    });
+  closeBtn.addEventListener("click", () => {
+      modal.classList.remove("is-active");
   });
+  modalBg.addEventListener("click", () => {
+      modal.classList.remove("is-active");
+  });
+  cancelBtn.addEventListener("click", () => {
+      modal.classList.remove("is-active");
+  });
+  
