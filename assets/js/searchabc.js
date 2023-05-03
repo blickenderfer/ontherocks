fd9d50ac8fb7f21b699e18fbecfdb4928971f2a0
blickenderfer <>
2023-05-02 21:27:55 -0400

    removed second js page

--- assets/js/searchabc.js
+++ assets/js/searchabc.js
@@ -1,44 +1,44 @@
-function makeList(letter){
-    console.log(letter.innerHTML)
-    modal.classList.add("is-active");
-    var modalTitle = document.querySelector(".modal-card-title");
-    let fetchUrl = `https://thecocktaildb.com/api/json/v1/1/search.php?s=${letter.innerHTML}`
-    fetch(fetchUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data.drinks);
-      })
-}
+// function makeList(letter){
+//     console.log(letter.innerHTML)
+//     modal.classList.add("is-active");
+//     var modalTitle = document.querySelector(".modal-card-title");
+//     let fetchUrl = `https://thecocktaildb.com/api/json/v1/1/search.php?s=${letter.innerHTML}`
+//     fetch(fetchUrl)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data.drinks);
+//       })
+// }
 
-var listBtn = document.querySelector(".list");
-var closeBtn = document.querySelector("#close-button");
-var modal = document.querySelector(".modal");
-var modalBg = document.querySelector(".modal-background");
-var cancelBtn = document.querySelector("#cancel");
+// var listBtn = document.querySelector(".list");
+// var closeBtn = document.querySelector("#close-button");
+// var modal = document.querySelector(".modal");
+// var modalBg = document.querySelector(".modal-background");
+// var cancelBtn = document.querySelector("#cancel");
   
-  listBtn.addEventListener("click", (event) => {
-      event.preventDefault();
-      console.log(event.target)
-      modal.classList.add("is-active");
-      var modalTitle = document.querySelector(".modal-card-title");
-      let fetchUrl = `https://thecocktaildb.com/api/json/v1/1/search.php?s=$a`
-      fetch(fetchUrl)
-          .then(function (response) {
-              return response.json();
-          })
-          .then(function (data) {
-              console.log(data.drinks);
-        })
-    });
-  closeBtn.addEventListener("click", () => {
-      modal.classList.remove("is-active");
-  });
-  modalBg.addEventListener("click", () => {
-      modal.classList.remove("is-active");
-  });
-  cancelBtn.addEventListener("click", () => {
-      modal.classList.remove("is-active");
-  });
+//   listBtn.addEventListener("click", (event) => {
+//       event.preventDefault();
+//       console.log(event.target)
+//       modal.classList.add("is-active");
+//       var modalTitle = document.querySelector(".modal-card-title");
+//       let fetchUrl = `https://thecocktaildb.com/api/json/v1/1/search.php?s=$a`
+//       fetch(fetchUrl)
+//           .then(function (response) {
+//               return response.json();
+//           })
+//           .then(function (data) {
+//               console.log(data.drinks);
+//         })
+//     });
+//   closeBtn.addEventListener("click", () => {
+//       modal.classList.remove("is-active");
+//   });
+//   modalBg.addEventListener("click", () => {
+//       modal.classList.remove("is-active");
+//   });
+//   cancelBtn.addEventListener("click", () => {
+//       modal.classList.remove("is-active");
+//   });
   
