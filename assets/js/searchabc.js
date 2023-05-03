465dfd230c34efdec59c15fdeeecc72588e0197d
Lykrin-X <amir.jones427@gmail.com>
2023-05-02 20:50:52 -0400

    unlink searchabc script

--- assets/js/searchabc.js
+++ assets/js/searchabc.js
@@ -2,7 +2,7 @@ function makeList(letter){
     console.log(letter.innerHTML)
     modal.classList.add("is-active");
     var modalTitle = document.querySelector(".modal-card-title");
-    let fetchUrl = `https://thecocktaildb.com/api/json/v1/1/search.php?s=${letter.innerHTML}`
+    let fetchUrl = `www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter.innerHTML}`
     fetch(fetchUrl)
         .then(function (response) {
             return response.json();
@@ -18,20 +18,20 @@ var modal = document.querySelector(".modal");
 var modalBg = document.querySelector(".modal-background");
 var cancelBtn = document.querySelector("#cancel");
   
-  listBtn.addEventListener("click", (event) => {
-      event.preventDefault();
-      console.log(event.target)
-      modal.classList.add("is-active");
-      var modalTitle = document.querySelector(".modal-card-title");
-      let fetchUrl = `https://thecocktaildb.com/api/json/v1/1/search.php?s=$a`
-      fetch(fetchUrl)
-          .then(function (response) {
-              return response.json();
-          })
-          .then(function (data) {
-              console.log(data.drinks);
-        })
-    });
+//   listBtn.addEventListener("click", (event) => {
+//       event.preventDefault();
+//       console.log(event.target)
+//       modal.classList.add("is-active");
+//       var modalTitle = document.querySelector(".modal-card-title");
+//       let fetchUrl = `https://thecocktaildb.com/api/json/v1/1/search.php?s=${letter.innerHTML}`
+//       fetch(fetchUrl)
+//           .then(function (response) {
+//               return response.json();
+//           })
+//           .then(function (data) {
+//               console.log(data.drinks);
+//         })
+    // });
   closeBtn.addEventListener("click", () => {
       modal.classList.remove("is-active");
   });
